b070a5945c0f55618563ca86b22eef1731814091
sajib-rana <sajibrana444@gmail.com>
2023-08-25 11:44:36 +0600

    server get and listen

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+  res.send("boss is sitting");
+});
+
+app.listen(port, () => {
+  console.log(`Bistro boss is sitting on port ${port}`);
+});
