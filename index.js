f82d62489b944a91fde69447309bca52ab846ec2
sajib-rana <sajibrana444@gmail.com>
2023-09-04 12:29:50 +0600

    dashboard section work

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ynhrrkd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -44,7 +44,7 @@ async function run() {
     // cart collection apis
     app.get("/carts", async (req, res) => {
       const email = req.query.email;
-
+       console.log(email)
       if (!email) {
         res.send([]);
       }
@@ -57,6 +57,13 @@ async function run() {
       const item = req.body;
       console.log(item);
       const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
 
