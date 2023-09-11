8e285fff58b9b013287520acee1d320ded7d3a2f
sajib-rana <sajibrana444@gmail.com>
2023-09-11 18:57:45 +0600

    user admin and verify token

--- index.js
+++ index.js
@@ -61,7 +61,20 @@ async function run() {
       res.send({ token });
     });
 
-    app.get("/users", async (req, res) => {
+    // Warning: use verifyJWT before using verifyAdmin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
+      }
+      next();
+    };
+
+    app.get("/users", verifyJWT,verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
